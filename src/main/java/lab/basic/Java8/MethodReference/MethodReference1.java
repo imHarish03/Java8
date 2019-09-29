4cc09622a83dc472928edab19b4e537c3b8b7403
imHarish03 <harishhari302@gmail.com>
2019-09-29 23:59:04 +0530

    Code optimization

--- src/main/java/lab/basic/Java8/MethodReference/MethodReference1.java
+++ src/main/java/lab/basic/Java8/MethodReference/MethodReference1.java
@@ -4,9 +4,23 @@ public class MethodReference1 {
 
 	public static void main(String[] args) {
 
-		Thread t = new Thread(() -> printMessage());
-		t = new Thread(MethodReference1::printMessage);
-		t.start();
+		Thread t1, t2, t3 = null;
+		// Classic
+		t1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				printMessage();
+			}
+		});
+		t1.start();
+
+		// Lambda
+		t2 = new Thread(() -> printMessage());
+		t2.start();
+
+		// Method Reference
+		t3 = new Thread(MethodReference1::printMessage);
+		t3.start();
 	}
 
 	public static void printMessage() {
