14a667f1f9962427de5d59435cf5600ffdacedd7
imHarish03 <harishhari302@gmail.com>
2019-12-28 23:47:20 +0530

    Implemented Optionals Flat method

--- src/main/java/lab/basic/Java8/Optionals/OptionalBasicExample.java
+++ src/main/java/lab/basic/Java8/Optionals/OptionalBasicExample.java
@@ -14,8 +14,8 @@ public class OptionalBasicExample {
 		System.out.println("Non-Empty Optional: Gender value : " + gender.get());
 		System.out.println("Empty Optional: " + Optional.empty());
 
-		System.out.println("ofNullable on Non-Empty Optional: " + Optional.ofNullable(answer1));
-		System.out.println("ofNullable on Empty Optional: " + Optional.ofNullable(answer2));
+		System.out.println("ofNullable on Non-Empty Optional: " + Optional.ofNullable(answer1).get());
+		System.out.println("ofNullable on Empty Optional: " + Optional.ofNullable(answer2).orElse("*****NULLL*****"));
 
 		// java.lang.NullPointerException
 		System.out.println("ofNullable on Non-Empty Optional: " + Optional.of(answer2));
